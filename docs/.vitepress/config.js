ded7265e2cea8661349bfac49854e8ffde0e6a55
liuchengjin <75836368@qq.com>
2022-10-25 23:03:20 +0800

    feat: 基础项目结构配置

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,13 @@ export default {
   title:'前端知识点及个人笔记',
   description:'前端知识点及个人笔记',
   themeConfig: {
-    siteTitle: "前端知识点及个人笔记",
-    logo: "/logo.png",
+    siteTitle: false,
+    logo:'/logo.png',
+    nav: [
+      { text: "个人笔记", link: "/guild/installation" },
+      { text: "面试题", link: "/examples/button/" },
+    ],
+    socialLinks: [{ icon: "github", link: "https://github.com/badlym" }],
   },
+
 }
